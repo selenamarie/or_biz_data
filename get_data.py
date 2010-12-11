aa85c7736c93899c329333fcaa154573019e89cd
Selena Deckelmann <selenamarie@gmail.com>
2010-12-11 00:17:43 +0000

    Small cleanup, added function for curl call

--- get_data.py
+++ get_data.py
@@ -17,13 +17,18 @@ class BizData:
 		except ValueError, e: 
 			print >>sys.stderr, 'Could not load JSON oboject from body_callback(): %', e
 
-b = BizData()
-c = pycurl.Curl()
-c.setopt(c.URL, 'http://api.scraperwiki.com/api/1.0/datastore/getdata?format=json&name=oregon_business_registry&limit=10')
-c.setopt(c.WRITEFUNCTION, b.body_callback)
-c.perform()
-c.close()
+	def get_data(self, url):
+		c = pycurl.Curl()
+		c.setopt(c.URL, url)
+		c.setopt(c.WRITEFUNCTION, self.body_callback)
+		c.perform()
+		c.close()
 
+
+url = 'http://api.scraperwiki.com/api/1.0/datastore/getdata?format=json&name=oregon_business_registry&limit=10'
+
+b = BizData()
+b.get_data(url)
 b.decode()
 		
 print b.json
