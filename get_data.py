f559b980f55be94edbb31dfde7be0d207fb3ff94
Selena Deckelmann <selenamarie@gmail.com>
2010-12-13 15:14:35 +0000

    Added json collections and get_all function for multiple URL calls with limit

--- get_data.py
+++ get_data.py
@@ -7,13 +7,21 @@ import pycurl
 class BizData:
 	def __init__(self):
 		self.contents = ''
+		self.archive = []
 
 	def body_callback(self, buf):
 		self.contents = self.contents + buf
 
+	def collect_json(self):
+		self.archive.append(self.json)
+
+	def reset_json(self):
+		self.archive = []
+
 	def decode(self):
 		try:
 			self.json = json.loads(self.contents);
+			self.collect_json()
 		except ValueError, e: 
 			print >>sys.stderr, 'Could not load JSON object from body_callback(): %', e
 
@@ -23,6 +31,16 @@ class BizData:
 		c.setopt(c.WRITEFUNCTION, self.body_callback)
 		c.perform()
 		c.close()
+
+	def get_all(self, url, limit):
+		c = pycurl.Curl()
+		url = url + "limit=%", limit
+		# reset contents for each and call decode on each
+		self.contents = ''
+		while self.contents != '[]': 
+			self.get(url)
+			if self.contents != '[]':
+				self.decode()
 
 	def search(self, param):
 		results = []
@@ -44,12 +62,6 @@ b = BizData()
 b.get(url)
 b.decode()
 
-#b.search('name')
-
 print b.searches(['name', 'source_url', 'registered_date'])
-
-
-
-
 
 
